ff183243082497291705ad6dbc51ea5df1fee707
Dmitry Lisachenko <xdemonx07@gmail.com>
2017-09-21 19:50:51 +0300

    conv-boost: variant support

--- include/sqlite3yaw/convert_boost.hpp
+++ include/sqlite3yaw/convert_boost.hpp
@@ -1,24 +1,32 @@
 #pragma once
 #include <sqlite3yaw/convert.hpp>
 #include <boost/optional.hpp>
+#include <boost/variant.hpp>
 
 namespace sqlite3yaw
 {
 	namespace convert
 	{
+		template <>
+		struct conv<boost::none_t>
+		{
+			static void put(boost::none_t, bool, ibind & b) { conv<std::nullptr_t>::put(nullptr, false, b); }
+			//static void get(boost::none_t & val, iquery & b) { val = boost::none; }
+		};
+
 		template <class Type>
 		struct conv<boost::optional<Type>>
 		{
-			typedef boost::optional<Type> Opt;
-			static void put(Opt const & val, bool temp, ibind & b)
+			typedef boost::optional<Type> optional;
+			static void put(const optional & val, bool temp, ibind & b)
 			{
 				if (val.is_initialized())
 					conv<Type>::put(val.get(), temp, b);
 				else
-					conv<std::nullptr_t>::put(nullptr, false, b);
+					conv<boost::none_t>::put(boost::none, false, b);
 			}
 
-			static void get(Opt & val, iquery & q)
+			static void get(optional & val, iquery & q)
 			{
 				if (q.get_type() == SQLITE_NULL)
 					val = boost::none;
@@ -27,11 +35,32 @@ namespace sqlite3yaw
 			}
 		};
 
-		template <>
-		struct conv<boost::none_t>
+
+		struct conv_variant_visitor : boost::static_visitor<void>
 		{
-			static void put(boost::none_t, bool, ibind & b) { conv<std::nullptr_t>::put(nullptr, false, b); }
-			//static void get(boost::none_t & val, iquery & b) { val = boost::none; }
+			ibind * bptr;
+			bool temp;
+
+			template <class Type>
+			void operator()(const Type & val) const
+			{
+				return conv<Type>::put(val, temp, *bptr);
+			}
+
+			conv_variant_visitor(ibind & b, bool temp)
+				: bptr(&b), temp(temp) {}
+		};
+
+
+		template <class ... Type>
+		struct conv<boost::variant<Type...>>
+		{
+			typedef boost::variant<Type...> variant;
+
+			static void put(const variant & var, bool temp, ibind & b)
+			{
+				return boost::apply_visitor(conv_variant_visitor(b, temp), var);
+			}
 		};
 	}
 }
